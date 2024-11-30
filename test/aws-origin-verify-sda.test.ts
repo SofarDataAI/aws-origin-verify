626acd28d39888bec23dfad7bbe862aac4ea81c3
Trung Dinh <xamarindevelopervietnam@outlook.com>
2024-11-30 09:09:22 +0700

    Init commit

---
+++ test/aws-origin-verify-sda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsOriginVerifySda from '../lib/aws-origin-verify-sda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-origin-verify-sda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsOriginVerifySda.AwsOriginVerifySdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
